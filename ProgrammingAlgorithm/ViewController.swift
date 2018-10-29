866ed5797643480d43a1e0715e6c25dd81cfb637
Tony Mu <tony.mu@fphcare.co.nz>
2018-10-30 11:32:20 +1300

    Initial Commit

---
+++ ProgrammingAlgorithm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammingAlgorithm
+//
+//  Created by Tony Mu on 30/10/18.
+//  Copyright © 2018 Tony Mu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
