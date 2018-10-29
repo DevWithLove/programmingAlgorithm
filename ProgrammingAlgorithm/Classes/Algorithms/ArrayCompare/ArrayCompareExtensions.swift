372e6cddc2de1f90f9baff910810f4f8776481e2
Tony Mu <tony.mu@fphcare.co.nz>
2018-10-30 11:50:52 +1300

    Initial commit

---
+++ ProgrammingAlgorithm/Classes/Algorithms/ArrayCompare/ArrayCompareExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  ArrayCompareExtensions.swift
+//  ProgrammingAlgorithm
+//
+//  Created by Tony Mu on 30/10/18.
+//  Copyright © 2018 Tony Mu. All rights reserved.
+//
+
+import UIKit
+
+extension Array where Element: Comparable {
+  
+  func containsSameElements(as other: [Element]) -> Bool {
+    return self.count == other.count && self.sorted() == other.sorted()
+  }
+  
+}
---
+++ ProgrammingAlgorithmTests/Algorithms/ArrayCompare/ArrayCompareWithComparebleElementsTests.swift
@@ -0,0 +1,32 @@
+//
+//  ArrayCompareWithComparebleElementsTests.swift
+//  ProgrammingAlgorithmTests
+//
+//  Created by Tony Mu on 30/10/18.
+//  Copyright © 2018 Tony Mu. All rights reserved.
+//
+
+import XCTest
+
+class ArrayCompareWithComparebleElementsTests: XCTestCase {
+
+    override func setUp() {
+
+    }
+
+    override func tearDown() {
+  
+    }
+
+    func testExample() {
+
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
